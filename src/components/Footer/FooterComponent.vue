38c29bd15a959a08214cad52ebce3d9739554db3
Headbwoi <headbwoi25@gmail.com>
2023-04-25 16:15:00 +0100

    mod footer

--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -5,7 +5,7 @@ import SocialComponent from "./SocialComponent.vue"
 <template>
   <div class="container px-5 md:px-[9.375rem]">
     <div
-      class="grid grid-cols-2 lg:grid-cols-12 gap-8 gap-y-20 md:gap-0 mt-11 w-full"
+      class="grid grid-cols-2 lg:grid-cols-12 gap-8 gap-y-20 md:gap-10 mt-11 md:mt-0 w-full"
     >
       <div class="flex flex-col col-span-2 lg:col-span-5 space-y-4">
         <img src="/logo-white.svg" alt="axia africa" class="w-20 block" />
@@ -17,35 +17,51 @@ import SocialComponent from "./SocialComponent.vue"
         <SocialComponent />
       </div>
       <div class="flex flex-col lg:col-span-2 gap-8 md:order-2">
-        <header class="font-bold">SCHOOLS</header>
-        <a href="/schools/product" class="text-lighestgrey text-sm font-medium"
+        <header class="font-bold md:text-lg">SCHOOLS</header>
+        <a
+          href="/schools/product"
+          class="text-lighestgrey text-sm lg:text-base font-medium"
           >School of Product</a
         >
-        <a href="/schools/data" class="text-lighestgrey text-sm font-medium"
+        <a
+          href="/schools/data"
+          class="text-lighestgrey text-sm lg:text-base font-medium"
           >School of Data</a
         >
         <a
           href="/schools/engineering"
-          class="text-lighestgrey text-sm font-medium"
+          class="text-lighestgrey text-sm lg:text-base font-medium"
           >School of Engineering</a
         >
       </div>
-      <div class="flex flex-col lg:col-span-2 gap-8 md:order-1">
-        <header class="font-bold">COMPANY</header>
-        <a href="/#onboard" class="text-lighestgrey text-sm font-medium"
+      <div class="flex flex-col lg:col-span-3 gap-8 md:order-1">
+        <header class="font-bold md:text-lg">COMPANY</header>
+        <a
+          href="/#onboard"
+          class="text-lighestgrey text-sm lg:text-base font-medium"
           >About</a
         >
-        <a href="/#" class="text-lighestgrey text-sm font-medium">Blog</a>
-        <a href="/faqs" class="text-lighestgrey text-sm font-medium">FAQs</a>
-        <a href="/application" class="text-lighestgrey text-sm font-medium"
+        <a href="/#" class="text-lighestgrey text-sm lg:text-base font-medium"
+          >Blog</a
+        >
+        <a
+          href="/faqs"
+          class="text-lighestgrey text-sm lg:text-base font-medium"
+          >FAQs</a
+        >
+        <a
+          href="/application"
+          class="text-lighestgrey text-sm lg:text-base font-medium"
           >Join Our Community</a
         >
-        <a href="/partners" class="text-lighestgrey text-sm font-medium"
+        <a
+          href="/partners"
+          class="text-lighestgrey text-sm lg:text-base font-medium"
           >Partnership</a
         >
       </div>
-      <div class="flex flex-col lg:col-span-2 gap-8 md:order-3">
-        <header class="font-bold text-base">CONTACT</header>
+      <div class="flex flex-col lg:col-span-1 gap-8 md:order-3">
+        <header class="font-bold md:text-lg text-base">CONTACT</header>
         <a href="mailto:axiaafrica@gmail.com" class="text-sm font-medium block"
           >axiaafrica@gmail.com</a
         >
