3c6c78a229856858c847cf3017e2c5b82b7f72bf
Headbwoi <headbwoi25@gmail.com>
2023-04-26 09:43:24 +0100

    .

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@ import { createRouter, createWebHistory } from "vue-router"
 import "./style.css"
 import App from "./App.vue"
 import HomePage from "./pages/Home/HomePage.vue"
-import SchoolOfProduct from "src/pages/Schools/SchoolOfProduct.vue"
-import SchoolOfData from "src/pages/Schools/SchoolOfData.vue"
-import SchoolOfEngineering from "src/pages/Schools/SchoolOfEngineering.vue"
+import SchoolOfProduct from "./pages/Schools/SchoolOfProduct.vue"
+import DataPage from "./pages/Schools/DataPage.vue"
+import SchoolOfEngineering from "./pages/Schools/SchoolOfEngineering.vue"
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "Home", component: HomePage },
     { path: "/schools/product", name: "Product", component: SchoolOfProduct },
-    { path: "/schools/data", name: "Data", component: SchoolOfData },
+    { path: "/schools/data", name: "Data", component: DataPage },
     {
       path: "/schools/engineering",
       name: "Engineering",
