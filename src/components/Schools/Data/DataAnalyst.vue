519315e4233e024190994920659379e169175593
Headbwoi <headbwoi25@gmail.com>
2023-04-26 09:30:48 +0100

    add schools of data page

---
+++ src/components/Schools/Data/DataAnalyst.vue
@@ -0,0 +1,46 @@
+<template>
+  <section
+    class="bg-primary w-full text-white px-5 py-24 flex items-center justify-center flex-col"
+  >
+    <h1 class="text-[1.75rem] md:text-[2.25rem] font-medium text-center">
+      Become a Data Analyst
+    </h1>
+    <p class="text-base max-w-[40rem] text-lighestgrey text-center mt-5">
+      Axia africa has designed for its students the programme to learn Data
+      Science in the schdeuled months and prepare them for jobs in the Data
+      Science field. Tools Include: SQL, Excel, PowerBI & Python.
+    </p>
+    <div class="flex flex-col md:flex-row text-lighestgrey gap-6 mt-12">
+      <div class="flex gap-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          class="w-4"
+        >
+          <path
+            d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
+            fill="#fff"
+          />
+        </svg>
+        <p class="font-medium">12 weeks Virtual Class</p>
+      </div>
+      <div class="flex gap-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          class="w-4"
+        >
+          <path
+            d="M96 32V64H48C21.5 64 0 85.5 0 112v48H448V112c0-26.5-21.5-48-48-48H352V32c0-17.7-14.3-32-32-32s-32 14.3-32 32V64H160V32c0-17.7-14.3-32-32-32S96 14.3 96 32zM448 192H0V464c0 26.5 21.5 48 48 48H400c26.5 0 48-21.5 48-48V192z"
+            fill="#fff"
+          />
+        </svg>
+        <p class="font-medium">Next class starts: May 17, 2023</p>
+      </div>
+    </div>
+
+    <a href="/enroll/product_designer" class="btn-secondary mt-16">
+      Apply Now
+    </a>
+  </section>
+</template>
---
+++ src/components/Schools/Data/HeroSection.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="w-full mt-20 mb-10">
+    <div class="container px-5 md:px-0 relative text-center">
+      <h1
+        class="headline text-[2rem] text-center font-medium text-darkish md:text-[2.8125rem]"
+      >
+        School of
+        <span class="gradient">Data</span>
+      </h1>
+      <p
+        class="text-darkgrey text-center md:text-lg mt-6 pb-28 font-medium mx-auto max-w-3xl"
+      >
+        Begin your Data learning career and more. Discover career opportunities
+        available for you. Speak to professionals and read through the materials
+        and the programme designed specifically for you.
+      </p>
+
+      <div class="flex items-center justify-center gap-5">
+        <div class="flex h-14">
+          <img src="/images/reviews/0.png" alt="person 1" />
+          <img src="/images/reviews/1.png" alt="person 2" class="-ml-5" />
+          <img src="/images/reviews/2.png" alt="person 3" class="-ml-5" />
+        </div>
+        <p class="text-darkgrey md:text-lg">500+ Active Students</p>
+      </div>
+
+      <!-- doddle -->
+      <img
+        src="/images/doddle3.svg"
+        alt="fjjdbfhdf"
+        class="absolute left-0 bottom-0"
+      />
+      <img
+        src="/images/doddle4.svg"
+        alt="fhehf"
+        class="absolute right-0 bottom-0"
+      />
+    </div>
+  </section>
+</template>
