f93e17634427a2221989e4dd1099e9593cb0023a
Headbwoi <headbwoi25@gmail.com>
2023-04-26 08:45:08 +0100

    add router add homepage and schools page

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,5 @@
-<script setup>
-import HeaderComponent from "./components/Header/HeaderComponent.vue"
-import HeroSection from "./components/HeroSection.vue"
-import CoursesSection from "./components/CoursesSection.vue"
-import AdvisorSection from "./components/AdvisorSection.vue"
-import OnboardSection from "./components/OnboardSection.vue"
-import DiscoverSection from "./components/DiscoverSection.vue"
-import FosterKnowledgeSection from "./components/FosterKnowledgeSection.vue"
-import OurSuccessSection from "./components/OurSuccessSection.vue"
-import PartnerSection from "./components/PartnerSection.vue"
-import FooterComponent from "./components/Footer/FooterComponent.vue"
-</script>
+<script setup></script>
 
 <template>
-  <HeaderComponent />
-
-  <main class="relative pt-28">
-    <HeroSection />
-    <CoursesSection />
-    <AdvisorSection />
-    <OnboardSection />
-    <DiscoverSection />
-    <FosterKnowledgeSection />
-    <OurSuccessSection />
-    <PartnerSection />
-  </main>
-
-  <footer
-    id="footer"
-    class="bg-primary text-white p-5 py-12 md:pt-24 md:pb-14 w-full mx-auto"
-  >
-    <FooterComponent />
-  </footer>
+  <RouterView />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import { createApp } from "vue"
-import App from "./App.vue"
-
+import { createRouter, createWebHistory } from "vue-router"
 import "./style.css"
+import App from "./App.vue"
+import HomePage from "./pages/Home/HomePage.vue"
+import SchoolOfProduct from "./pages/Schools/SchoolOfProduct.vue"
+import SchoolOfData from "./pages/Schools/SchoolOfData.vue"
+import SchoolOfEngineering from "./pages/Schools/SchoolOfEngineering.vue"
 
-createApp(App).mount("#app")
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "Home", component: HomePage },
+    { path: "/schools/product", name: "Product", component: SchoolOfProduct },
+    { path: "/schools/data", name: "Data", component: SchoolOfData },
+    {
+      path: "/schools/engineering",
+      name: "Engineering",
+      component: SchoolOfEngineering,
+    },
+  ],
+})
+
+createApp(App).use(router).mount("#app")
