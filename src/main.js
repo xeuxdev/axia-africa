d37b160ce96298fc67a163e88a48bdcc6764b0b8
Headbwoi <headbwoi25@gmail.com>
2023-04-26 10:00:41 +0100

    add product school

--- src/main.js
+++ src/main.js
@@ -6,13 +6,14 @@ import App from "./App.vue"
 import HomePage from "./pages/Home/HomePage.vue"
 import SchoolOfData from "./pages/Schools/SchoolOfData.vue"
 import SchoolOfEngineering from "./pages/Schools/SchoolOfEngineering.vue"
+import ProductSchool from "./pages/Schools/ProductSchool.vue"
 // import SchoolOfProduct from "./pages/Schools/SchoolOfProduct.vue"
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "Home", component: HomePage },
-    // { path: "/schools/product", name: "Product", component: SchoolOfProduct },
+    { path: "/schools/product", name: "Product", component: ProductSchool },
     { path: "/schools/data", name: "Data", component: SchoolOfData },
     {
       path: "/schools/engineering",
